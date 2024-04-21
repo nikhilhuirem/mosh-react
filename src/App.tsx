e525058002b3bb925c7a7610232d941f3970cf96
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-04-21 12:20:26 +0530

    added error and loading state using axios fetching

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ import ListGroups from "./components/ListGroups"
 import FilterCategory from "./components/FilterCategory"
 import categories from "./components/categories"
 import ProductList from "./products/components/ProductList"
+import axios, { CanceledError } from "axios"
 
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
   // const [selectedCategory, setSelectedCategory] = useState('');
@@ -18,11 +24,35 @@ function App() {
   // ])
   // const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses;
 
-  const [category, setCategory] = useState('');
+  // const [category, setCategory] = useState('');
+
+  const [users ,setUsers] = useState<User []>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    console.log("UseEffect from app.tsx", {category})
+    const controller  = new AbortController();
+    setIsLoading(true)
+    axios.get<User []>('https://jsonplaceholder.typicode.com/users').then(res => {
+      setUsers(res.data) 
+      setIsLoading(false) 
+    } 
+  ).catch(err =>{if (err instanceof CanceledError) return 
+    setError(err.message)
+    setIsLoading(false)
+    })
+
+    return () => {controller.abort()}
   }, [])
   return (
+    <>
+      { error && <p className="text-danger">{error}</p>}
+      { isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+    
+    
     // <div>
     //   <div className="mb-5">
     //     <ExpenseForm onSubmit={expense => setExpenses([...expenses, { ...expense, id: expenses.length + 1}])} />
@@ -32,14 +62,15 @@ function App() {
     //   </div>
     //   <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id ))} />
     // </div>
-    <div>
-      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="clothing">Clothing</option>
-        <option value="electronics">Electronics</option>
-      </select>
-      <ProductList category={category} />
-    </div>
+
+    // <div>
+    //   <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
+    //     <option value=""></option>
+    //     <option value="clothing">Clothing</option>
+    //     <option value="electronics">Electronics</option>
+    //   </select>
+    //   <ProductList category={category} />
+    // </div>
   )
 }
 
