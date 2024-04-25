9a53aea1bcd58a8c49ee69171f1ad847345b0c30
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-04-25 23:40:49 +0530

    changes

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,3 @@
-
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface Expense {
     id: number;
     description: string;
